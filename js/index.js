35ed930ab69e6b3097238d8b168fb64294b21672
Kazi Rahat <kazirahat1007@outlook.com>
2022-09-03 07:15:47 +0600

    load category_name and display them in the html file

--- js/index.js
+++ js/index.js
@@ -2,7 +2,24 @@ const loadCategories = async () => {
     const url = "https://openapi.programming-hero.com/api/news/categories";
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data.news_category);
+    displayCategories(data.data.news_category);
 };
 
+const displayCategories = (categories) => {
+    const ul = document.getElementById("categories-list");
+    categories.forEach((category) => {
+        const a = document.createElement("a");
+        a.innerHTML = `
+        <li class="category-item" onclick="loadNews('${category.category_id}')">${category.category_name}</li>
+        `;
+        ul.appendChild(a);
+    });
+};
+
+const loadNews = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+};
 loadCategories();
