3dd2b6bcd7922c6ac5165e46c396922c7c01413e
Kazi Rahat <kazirahat1007@outlook.com>
2022-09-03 06:38:28 +0600

    Load Categories

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
+const loadCategories = async () => {
+    const url = "https://openapi.programming-hero.com/api/news/categories";
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data.news_category);
+};
 
+loadCategories();
